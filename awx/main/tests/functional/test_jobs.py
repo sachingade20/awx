d11439e4df12f313b8e114e58a3d592281ab0a3f
Matthew Jones <matburt@redhat.com>
2017-10-10 12:32:32 -0400

    Removing old total_capacity instance manager property

--- awx/main/tests/functional/test_jobs.py
+++ awx/main/tests/functional/test_jobs.py
@@ -1,5 +1,4 @@
-from awx.main.models import Job, Instance
-from django.test.utils import override_settings
+from awx.main.models import Job
 import pytest
 
 import json
@@ -14,16 +13,6 @@ def test_orphan_unified_job_creation(instance, inventory):
     assert job2.name == 'hi world'
     assert job.job_type == job2.job_type
     assert job2.launch_type == 'relaunch'
-
-
-@pytest.mark.django_db
-def test_job_capacity_and_with_inactive_node():
-    Instance.objects.create(hostname='test-1', capacity=50)
-    assert Instance.objects.total_capacity() == 50
-    Instance.objects.create(hostname='test-2', capacity=50)
-    assert Instance.objects.total_capacity() == 100
-    with override_settings(AWX_ACTIVE_NODE_TIME=0):
-        assert Instance.objects.total_capacity() < 100
 
 
 @pytest.mark.django_db
